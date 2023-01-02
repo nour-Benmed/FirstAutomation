8bac20f6e7df73ce268f53f6c16517d2ece8101f
nour-Benmed <nourbenmed32@gmail.com>
2023-01-02 20:37:02 +0000

    Fist commit

---
+++ src/test/java/com/e2eTests/automatedTests/authentication/pageObjects/AuthenticationPage.java
@@ -0,0 +1,8 @@
+package com.e2eTests.automatedTests.authentication.pageObjects;
+
+import com.e2eTests.automatedTests.utilis.BasePage;
+
+public class AuthenticationPage extends BasePage {
+
+
+}
---
+++ src/test/java/com/e2eTests/automatedTests/authentication/stepDefinitions/AuthenticationStepDefinitions.java
@@ -0,0 +1,9 @@
+package com.e2eTests.automatedTests.authentication.stepDefinitions;
+
+import org.junit.Assert;
+
+import com.e2eTests.automatedTests.authentication.pageObjects.AuthenticationPage;
+
+public class AuthenticationStepDefinitions {
+
+}
